d379daf43bf3949da07b460c47fb2d7ad5c134a0
alban.lagragui <alban.lagragui@gmail.com>
2023-12-14 19:06:32 +0100

    :zap: Classe ajoutPion permettant d'ajouter un pion

--- src/main/java/alban/puissance/puissancequatre/PuissanceQuatreController.java
+++ src/main/java/alban/puissance/puissancequatre/PuissanceQuatreController.java
@@ -3,6 +3,8 @@ package alban.puissance.puissancequatre;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -29,8 +31,12 @@ public class PuissanceQuatreController {
         stage.close();
     }
 
-    protected void ajouterPion(){
-
+    protected void ajouterPion(int col, int row, String nomImage){
+        Image image = new Image(getClass().getResourceAsStream("/src/main/resources/alban.puissance.images/" + nomImage));
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(100);
+        imageView.setFitWidth(100);
+        grilleJeu.add(imageView, col, row);
     }
     @FXML
     protected void grilleJeu(){
